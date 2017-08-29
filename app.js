33e0d07289c76bc785e43b8fee805b28feb6cf44
Sam Steele <samsteele@User-Mac.local>
2017-08-29 16:09:01 -0400

    4:00 Should work. Error with mongo.

--- app.js
+++ app.js
@@ -1,25 +1,54 @@
 const fs = require('fs');
+const port = 3000;
 const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
-mongoose.Promise = require('bluebird');
+const bodyParser = require('body-parser');
 const mustacheExpress = require('mustache-express');
 
+const Videogame = require('./collection')
+
+const mongoURL = 'mongodb://localhost:27017/videogames';
+mongoose.connect(mongoURL, {useMongoClient: true});
+mongoose.Promise = require('bluebird');
+
 const app = express();
-mongoose.connect('mongodb://localhost:27017/games_list');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
 app.set('view engine', 'mustache')
-app.set('layout', 'layout')
+// app.set('layout', 'layout')
 
 app.use('/static', express.static('static'));
 
-app.get('/new/:name', function(req, res){
-   const game = new Boardgame({name: req.param.name})
-   game.save
+//routing begin!
+
+app.get('/', function (req, res) {
+  Videogame.find().then(function (Videogame) {
+    res.render('index', {Videogame: Videogame});
+  })
+})
+
+app.get('/new', function(req, res){
+  res.render('new');
+ })
+
+ app.post('/new', function(req, res){
+   Videogame.create(req.body).then(function(){
+     res.redirect('/');
+   })
+})
+
+app.get('/:id/', function(req, res){
+   Videogame.findOne({_id: req.params.id}).then(function(videogame){
+     res.render('videogame', {videogame: videogame})
+   })
  })
 
  app.listen(port, function() {
    console.log('Example listening on port 3000')
  })
+
+module.exports = app;
--- collection.js
+++ collection.js
@@ -4,19 +4,19 @@ const collectionSchema = new mongoose.Schema({
   name: {type: String, required: true, unique: true},
   release_date: Number,
   genre: String,
-  rating: String,
+  IGNrating: Number,
+  ESRBrating: String,
   publisher: String,
-  platform: String,
-  rating: Number,
+  platforms: [String],
   price: Number,
   players: {
     min: {type: Number, default: 1},
     max: Number,
     multiPlayer: Boolean
-  }
+  },
   characters: [{name: String, npc: Boolean, enemy: Boolean}],
 })
 
-const Videogames = mongoose.model('Videogames', collectionSchema);
+const Videogame = mongoose.model('Videogame', collectionSchema);
 
-module.export = 
+module.exports = Videogame;
